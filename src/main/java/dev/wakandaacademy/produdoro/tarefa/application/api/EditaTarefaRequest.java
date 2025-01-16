18372cd9b80f5691445675493a7c224360030b11
sysoutemanuelhenrique <emanuel2017.henrique@gmail.com>
2025-01-16 17:28:32 -0300

    Feat: Edita tarefa request

---
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/application/api/EditaTarefaRequest.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.produdoro.tarefa.application.api;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class EditaTarefaRequest {
+	
+	@NotBlank
+	@Size (message = "Campo Descrição Tarefa Não Pode Estar Vazio", max = 255, min = 1)
+	private String descricao;
+	
+
+}
