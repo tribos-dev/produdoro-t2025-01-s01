9e211ddfd484cff0fc9efa2670077f782e3a6700
sysoutemanuelhenrique <emanuel2017.henrique@gmail.com>
2025-01-22 17:11:16 -0300

    Feat: Adiciona testes unitários

--- src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
+++ src/test/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioApplicationServiceTest.java
@@ -9,16 +9,48 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.UUID;
 
-
-
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import dev.wakandaacademy.produdoro.DataHelper;
+import dev.wakandaacademy.produdoro.config.security.service.TokenService;
+import dev.wakandaacademy.produdoro.usuario.application.repository.UsuarioRepository;
+import dev.wakandaacademy.produdoro.usuario.domain.StatusUsuario;
+import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 
 
-
-
+@ExtendWith(MockitoExtension.class)
 class UsuarioApplicationServiceTest {
+	
+	@InjectMocks
+	 UsuarioApplicationService usuarioApplicationService;
+	
+	@Mock
+	UsuarioRepository usuarioRepository;
+	
 
+	
+	@Test
+	void testPausaCurta() {
+		
+		Usuario usuario = DataHelper.createUsuario();
+		
+		when(usuarioRepository.buscaUsuarioPorEmail(anyString())).thenReturn(usuario);
+		when(usuarioRepository.buscaUsuarioPorId(any())).thenReturn(usuario);
+		
+		usuarioApplicationService.statusPausaCurta(usuario.getEmail(), usuario.getIdUsuario());
+		assertEquals(StatusUsuario.PAUSA_CURTA, usuario.getStatus());
+		verify(usuarioRepository, times(1)).salva(usuario);
+		
+		
+
+	}
+	
+	
 
 	}
