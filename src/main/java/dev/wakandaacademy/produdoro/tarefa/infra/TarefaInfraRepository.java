da6e589706c5a2bf8ef92331ac0fa134861e9422
Ronan Lima <ronanlimasilva@hotmail.com>
2025-01-22 21:35:00 -0300

    feat: implementa a camada repository

--- src/main/java/dev/wakandaacademy/produdoro/tarefa/infra/TarefaInfraRepository.java
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/infra/TarefaInfraRepository.java
@@ -6,6 +6,10 @@ import dev.wakandaacademy.produdoro.tarefa.domain.Tarefa;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
@@ -19,6 +23,7 @@ import java.util.UUID;
 public class TarefaInfraRepository implements TarefaRepository {
 
     private final TarefaSpringMongoDBRepository tarefaSpringMongoDBRepository;
+    private final MongoTemplate mongoTemplate;
 
     @Override
     public Tarefa salva(Tarefa tarefa) {
@@ -60,4 +65,16 @@ public class TarefaInfraRepository implements TarefaRepository {
     public List<Tarefa> buscaTarefasPorUsuario(UUID idUsuario) {
         return tarefaSpringMongoDBRepository.findAllByIdUsuario(idUsuario);
     }
+
+    @Override
+    public void desativaTarefa(UUID idUsuario) {
+        log.info("[inicia] TarefaInfraRepository - desativarTarefa");
+        Query query = new Query(
+                Criteria.where("StatusAtivacao").is("ATIVA")
+                        .and("idUsuario").is(idUsuario));
+        Update update = new Update().set("StatusAtivacao", "INATIVA");
+        mongoTemplate.updateMulti(query, update, Tarefa.class);
+        log.info("[finaliza] TarefaInfraRepository - desativarTarefa");
+    }
+
 }
